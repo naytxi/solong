f23cb4ee4efe9204ce68cbb73483a608e2412d5f
Nahia Otegui Beldarrain <naotegui@c3r8s6.42urduliz.com>
2024-05-03 12:28:28 +0200

    forth proyect

---
+++ so_long.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naotegui <naotegui@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 13:10:46 by naotegui          #+#    #+#             */
+/*   Updated: 2024/04/23 13:22:30 by naotegui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
+# include "ft_printf"/ft_printf.h"
+# include "minilibx/mlx.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdbool.h>
+
+
+#endif
